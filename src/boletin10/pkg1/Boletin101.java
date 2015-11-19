da877cee2203bb3dd1547605ac7554a47888a281
Manuel <Manuel@Mate-PC>
2015-11-19 12:23:09 +0100

    START

---
+++ src/boletin10/pkg1/Boletin101.java
@@ -0,0 +1,11 @@
+package boletin10.pkg1;
+public class Boletin101 {
+
+    public static void main(String[] args) {
+     Juego obj=new Juego();
+     obj.pregunta();
+             
+       
+    }
+    
+}
